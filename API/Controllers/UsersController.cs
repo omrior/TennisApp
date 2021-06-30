14223e610e41118e2dc63cf1f4b7a4b8311fd6b7
omrior <55718926+omrior@users.noreply.github.com>
2021-06-30 15:32:01 +0300

    End of section 2.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) 
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        /*[HttpPost]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() 
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }*/
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        public string UserName { get; set; }
+        //public string City { get; set; }
+        public int Level { get; set; }
+    }
+}
